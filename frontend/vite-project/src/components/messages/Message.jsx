f6b521a74e7a0e8d425ec73b86a59c7d7bda3813
shivam <shivamsk682002@gmail.com>
2024-12-16 22:04:55 +0530

    ui done

---
+++ frontend/vite-project/src/components/messages/Message.jsx
@@ -0,0 +1,16 @@
+
+const Message = ({ message }) => {
+	
+	return (
+		<div className={`chat chat-end`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png' />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white bg-blue-500`}>hi! What's up</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>10:23</div>
+		</div>
+	);
+};
+export default Message;
